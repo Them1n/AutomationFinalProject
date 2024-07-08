18ef49f3e457a12813ac57ca874dc58be6f98e53
Them1n <s1nedjkeee@gmail.com>
2024-07-08 22:38:46 +0300

    corrections

--- src/test/java/BaseTest.java
+++ src/test/java/BaseTest.java
@@ -20,13 +20,13 @@ public abstract class BaseTest {
     @BeforeMethod
     public void setUp() {
         driver = new ChromeDriver();
-        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(3));
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
         driver.manage().window().maximize();
         driver.get("https://football.ua/");
         homePage = new HomePage(driver);
         registerPage = new RegisterPage(driver);
         loginPage = new LoginPage(driver);
-        wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+        wait = new WebDriverWait(driver, Duration.ofSeconds(15));
     }
 
     @AfterMethod
